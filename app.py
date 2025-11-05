4dfb032911e23c3851979c1a1b1fc336cba10ec4
anjanadeashan <anjanaadeashan2@gmail.com>
2025-11-05 13:00:23 +0530

    Set up Flask application with waitress server and update port configuration; add .gitignore and requirements file

--- app.py
+++ app.py
@@ -48,4 +48,10 @@ def delete_file(filename):
     return jsonify({'success': False}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
+
+if __name__ == "__main__":
+    from waitress import serve
+    import os
+    port = int(os.environ.get("PORT", 5001))
+    serve(app, host="0.0.0.0", port=port)
